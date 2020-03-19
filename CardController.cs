f40dc349a5b0ef71ed65d38251bd62e802f39dd6
Icetail1 <51836525+Icetail1@users.noreply.github.com>
2020-03-19 11:11:18 +0900

    Update and rename CardInstance.cs to CardController.cs

--- CardController.cs
+++ CardController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CardInstance : MonoBehaviour {
+public class CardController : MonoBehaviour {
     //获取图片组件，设置图片  
     public Image image;
     //获取Prefab上绑定过的指定的图片

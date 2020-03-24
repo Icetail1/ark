f151e80ab0c1aecb7d7584a2665af2d9710a3a25
Icetail1 <51836525+Icetail1@users.noreply.github.com>
2020-03-24 10:29:11 +0900

    Update Lucyfa.cs

--- Lucyfa.cs
+++ Lucyfa.cs
@@ -35,9 +35,49 @@ public class Lucyfa : MonoBehaviour
             {
             PlayTsukiSasu(); 
             }
+            if (card.CardID == 8)
+            {
+            PlayPoFuChenZhou(); 
+            }
+            if (card.CardID == 10)
+            {
+            PlayAtk_1(); 
+            }
+            if (card.CardID == 11)
+            {
+            PlayAtk_2(); 
+            }
+            if (card.CardID == 12)
+            {
+            PlayAtk_3; 
+            }
       
     }
     public void PlayTsukiSasu()
+    {
+      Present_HP = Present_HP - (playermanager.instance.persent_HP * 0.5 * 3);
+      playermanager.instance.persent_HP = playermanager.instance.persent_HP * 0.5;
+      UIUpdate();
+    }
+    public void PlayPoFuChenZhou()
+    {
+      Present_HP = Present_HP - (Buffmanager.instance.BattleBeiShui * (Present_HP-playermanager.instance.persent_HP)*9);
+      playermanager.instance.turnoverdie = true;
+      UIUpdate();
+    }
+    public void PlayAtk_1()
+    {
+      Present_HP = Present_HP - (playermanager.instance.persent_HP * 0.5 * 3);
+      playermanager.instance.persent_HP = playermanager.instance.persent_HP * 0.5;
+      UIUpdate();
+    }
+    public void PlayAtk_2()
+    {
+      Present_HP = Present_HP - (playermanager.instance.persent_HP * 0.5 * 3);
+      playermanager.instance.persent_HP = playermanager.instance.persent_HP * 0.5;
+      UIUpdate();
+    }
+    public void PlayAtk_3()
     {
       Present_HP = Present_HP - (playermanager.instance.persent_HP * 0.5 * 3);
       playermanager.instance.persent_HP = playermanager.instance.persent_HP * 0.5;

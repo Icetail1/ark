5196fbec868d21b4c304a22709db2c435da23577
Icetail1 <51836525+Icetail1@users.noreply.github.com>
2020-03-24 09:37:13 +0900

    Create add.cs

---
+++ add.cs
@@ -0,0 +1,27 @@
+if(CardMannagement.Instance.NowChooseCard.CardID == 4 
+|| CardMannagement.Instance.NowChooseCard.CardID == 8
+|| CardMannagement.Instance.NowChooseCard.CardID == 10
+|| CardMannagement.Instance.NowChooseCard.CardID == 11
+|| CardMannagement.Instance.NowChooseCard.CardID == 12){
+检测
+if true→
+(if name = boss CardOnthis
+DO3d
+movetolosscard=todo
+destroy this card)else(
+return to gameobject first place
+)
+
+
+}else(
+if(Input.mousePosition.y>220)
+{
+CardOn
+DO3d
+movetolosscard=todo
+destroy this card
+
+}else(
+return to gameobject first place
+)
+)

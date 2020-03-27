d168608fa129f3aef74989c02ea234cd7a27df5d
Icetail1 <51836525+Icetail1@users.noreply.github.com>
2020-03-27 20:36:53 +0900

    Add files via upload

---
+++ AI/LucyfaAtk.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using BehaviorDesigner.Runtime.Tasks;
+using BehaviorDesigner.Runtime;
+
+public class LucyfaAtk : Conditional
+{
+
+    public GameObject Lucyfa;
+
+    public override void OnStart()
+    {
+        Lucyfa = GameObject.Find("boss");
+        base.OnStart();
+    }
+
+    public override TaskStatus OnUpdate()
+    {
+        Lucyfa.GetComponent<Lucyfa>().ATTKa();
+        UIManager.Instance.isTurnOver = false;
+        Lucyfa.GetComponent<Lucyfa>().ATK = Random.Range(1, 20);
+        PlayManager.Instance.turnCost = PlayManager.Instance.limitCost;
+        PlayManager.Instance.Defence = 0;
+        BuffManager.Instance.Cut = 0;
+        return TaskStatus.Success;
+
+
+    }
+}
---
+++ AI/TRUNOVER.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using BehaviorDesigner.Runtime.Tasks;
+using BehaviorDesigner.Runtime;
+
+public class TRUNOVER : Conditional
+{
+
+
+    public override TaskStatus OnUpdate()
+    {
+        if(UIManager.Instance.isTurnOver)
+        {
+            return TaskStatus.Success;
+        }
+        else
+        {
+            return TaskStatus.Failure;
+        }
+
+
+    }
+}
